b8571b2d15d060444c674880beca31ac358b8628
Gabs Monteiro <gmonteeeiro@gmail.com>
2021-04-01 09:49:03 -0300

    Change bot.py to use WhatsappBot class

--- bot.py
+++ bot.py
@@ -1,15 +1,4 @@
-from twilio.rest import Client
-from decouple import config
+from whatsapp_bot import WhatsappBot
 
-account_sid = config('TWILIO_ACCOUNT_ID')
-auth_token = config('TWILIO_AUTH_TOKEN')
-
-client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-                              body='Mensagem enviada pelo python',
-                              from_='whatsapp:+14155238886',
-                              to='whatsapp:+555196540271'
-                          )
-
-print(message.sid)
+whatsapp = WhatsappBot()
+whatsapp.send_message('+555196540271', 'outra mensagem mais legal')
